b51c1dd947ee8b0730c46872bdfbc5084a3786c5
Patrick Gomes <patrickgomes@gmail.com>
2017-11-08 16:32:33 -0600

    added custom and authorization and authentication experiments completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { GithubFollowersService } from './services/github-followers.service';
-import { DataService } from './services/data.service';
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { MockBackend } from '@angular/http/testing';
+import { fakeBackendProvider } from './helpers/fake-backend';
 import { ErrorHandler, Component } from '@angular/core';
 import { AppErrorhandler } from './common/app-error-handler';
-import { PostsService } from './services/posts.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpModule, BaseRequestOptions } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { NoAccessComponent } from './no-access/no-access.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course/course.component';
-import { CoursesService } from './courses/courses.service';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
@@ -23,6 +26,13 @@ import { HomeComponent } from './home/home.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+import { CoursesService } from './courses/courses.service';
+import { AuthService } from './services/auth.service';
+import { OrderService } from './services/order.service';
+import { DataService } from './services/data.service';
+import { PostsService } from './services/posts.service';
+import { GithubFollowersService } from './services/github-followers.service';
 
 @NgModule({
   declarations: [
@@ -38,7 +48,10 @@ import { NavbarComponent } from './navbar/navbar.component';
     GithubFollowersComponent,
     GithubProfileComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AdminComponent,
+    LoginComponent,
+    NoAccessComponent
   ],
   imports: [
     BrowserModule,
@@ -47,9 +60,14 @@ import { NavbarComponent } from './navbar/navbar.component';
     HttpModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'followers', component: GithubFollowersComponent },
       { path: 'followers/:id', component: GithubProfileComponent },
+      { path: 'followers', component: GithubFollowersComponent, canActivate: [AuthGuard] },
       { path: 'posts', component: PostsComponent },
+      { path: 'admin', 
+        component: AdminComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard]  },
+      { path: 'login', component: LoginComponent },
+      { path: 'no-access', component: NoAccessComponent },
       { path: '**', component: HomeComponent }
     ])
   ],
@@ -58,8 +76,17 @@ import { NavbarComponent } from './navbar/navbar.component';
     DataService,
     CoursesService,
     GithubFollowersService,
-    { provide: ErrorHandler, useClass: AppErrorhandler }
+    OrderService,
+    AuthService,
+    AuthGuard,
+    AdminAuthGuard,
+    // { provide: ErrorHandler, useClass: AppErrorhandler },
+    //For mock back-end.
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
   ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
